7e544315e79d504952ac6b64b8c5b57435a8b285
jonathas <jonathaslkc@hotmail.com>
2021-11-27 16:38:56 -0300

    Nova atualizacao app template para melhorar visual do portal

--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,5 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpRequest
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    return render(request, 'index.html')
